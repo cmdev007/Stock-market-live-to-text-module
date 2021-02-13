30ffd1d936f1b93252d99c840c89cec42d28a090
Preet Patel <preet.makz@gmail.com>
2021-02-13 23:23:39 +0530

    SHELL: Remove processed wave file

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ do
         ffmpeg -i data$COUNTER.mp4 -vn -acodec pcm_s16le -ar 44100 -ac 2 $(pwd)/audio/data$COUNTER.wav
         rm data$COUNTER.mp4
         python speech-to-text.py --file $(pwd)/audio/data$COUNTER.wav
+        rm $(pwd)/audio/data$COUNTER.wav
         let "COUNTER+=1"
 done
