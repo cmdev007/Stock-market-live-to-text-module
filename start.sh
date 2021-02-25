738229b0994808700a32edbce4e56adcefaf810a
Preet Patel <preet.makz@gmail.com>
2021-02-25 14:29:50 +0530

    SHELL: Updated shell script to encorporate new standard of pipe
    Co-authored-by: Abhiram Shibu <abhiramshibu1998@gmail.com>

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 COUNTER=0
-while :
-do
-        timeout 5 youtube-dl --no-part $1 --output data$COUNTER.mp4
-        sleep 5
-        ffmpeg -i data$COUNTER.mp4 -vn -acodec pcm_s16le -ar 44100 -ac 2 $(pwd)/audio/data$COUNTER.wav
-        rm data$COUNTER.mp4
-        python speech-to-text.py --file $(pwd)/audio/data$COUNTER.wav
-        rm $(pwd)/audio/data$COUNTER.wav
-        let "COUNTER+=1"
-done
+# while :
+# do
+youtube-dl $1 -o - | ffmpeg -i - -f wav - | pv | python speech-to-text.py
+# done
