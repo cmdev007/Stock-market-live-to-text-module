9ad3b6da739160ad316553a0b58e60bf45776f70
Preet Patel <preet.makz@gmail.com>
2021-02-25 14:35:46 +0530

    PYTHON: implemented pipe inflow and wave file's binary reading from pipe
    Co-authored-by: Abhiram Shibu <abhiramshibu1998@gmail.com>

--- speech-to-text.py
+++ speech-to-text.py
@@ -2,30 +2,36 @@
 
 import speech_recognition as sr
 import sys
+import os
+header = sys.stdin.buffer.read(78)
+while(1):
+    #data = sys.stdin.buffer.read(882000) #5 sec
+    data = sys.stdin.buffer.read(3528000) #20 sec
+    f = open("/tmp/inter.wav", "wb")
+    f.write(header)
+    f.write(data)
+    f.close()
+    os.system("ffmpeg -y -i /tmp/inter.wav -f wav /tmp/inter_f.wav 2> /dev/null")
 
-#def argParse():
-argv = sys.argv
-#return args
+    File="/tmp/inter_f.wav"
+    AUDIO_FILE = File
+    r = sr.Recognizer()
 
-#args = argParse()
-File=argv[-1]
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)
 
-AUDIO_FILE = (File) 
-  
-r = sr.Recognizer() 
-  
-with sr.AudioFile(AUDIO_FILE) as source: 
-    audio = r.record(source)   
-  
-try: 
-    Data=r.recognize_google(audio)
-    file1 = open("textData.txt","a")#append mode 
-    file1.write(Data+"\n") 
-    file1.close()
-    print("The audio file contains: " + r.recognize_google(audio)) 
-  
-except sr.UnknownValueError: 
-    print("Google Speech Recognition could not understand audio") 
-  
-except sr.RequestError as e: 
-    print("Could not request results from Google Speech  Recognition service; {0}".format(e))
+    try:
+        print("#######################################################################################")
+        print("Recognizing Text...")
+        Data=r.recognize_google(audio)
+        file1 = open("textData.txt","a")#append mode
+        file1.write(Data+" ")
+        file1.close()
+        print("#######################################################################################")
+        print("The audio file contains: " + Data)
+
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech  Recognition service; {0}".format(e))
